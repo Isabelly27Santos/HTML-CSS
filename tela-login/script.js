cbf59a7946ff1eecb9d23fbce2677183ec3021a0
Isabelly Santos <isabelly27js@gmail.com>
2026-09-14 00:39:06 -0300

    tela-login

--- tela-login/script.js
+++ tela-login/script.js
@@ -6,3 +6,27 @@ function mostrarTela(id) {
     // Coloca como ativa o id da classe selecionada
     document.getElementById(id).classList.add('ativa');
 }
+
+const olhos = document.querySelectorAll('.olho');
+
+olhos.forEach(olho => {
+
+    olho.addEventListener('click', () => {
+
+        const senha = olho.previousElementSibling;
+
+        if (senha.type === 'password') {
+
+            senha.type = 'text';
+            olho.className = 'olho fa-regular fa-eye';
+
+        } else {
+
+            senha.type = 'password';
+            olho.className = 'olho fa-regular fa-eye-slash';
+
+        }
+
+    });
+
+});
